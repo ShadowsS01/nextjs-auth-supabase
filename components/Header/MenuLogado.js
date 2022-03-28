0e1d7e4ac4823bad4f6e25f4a4b0f5898773e833
DKsHs <nicolas.contiero198@gmail.com>
2022-03-28 18:49:23 -0300

    Melhorias no Header.

--- components/Header/MenuLogado.js
+++ components/Header/MenuLogado.js
@@ -48,7 +48,7 @@ const MenuLogado = ({ profile }) => (
                 {({ active }) => (
                   <Link href="/profile">
                     <a className={classNames(
-                      active ? '' : 'rounded-sm hover:text-blue-600 hover:bg-gray-300 dark:hover:bg-blue-600/10 duration-300',
+                      active ? '' : 'rounded-sm hover:text-blue-600 hover:bg-blue-300 dark:hover:text-blue-300 dark:hover:bg-blue-600/30 duration-300',
                       'block px-4 py-2 text-sm'
                     )}>
                       Seu perfil
@@ -62,7 +62,7 @@ const MenuLogado = ({ profile }) => (
                     <Link href="/community">
                       <a
                         className={classNames(
-                          active ? '' : 'flex sm:hidden font-bold justify-center bg-[length:400px_400px] bg-gradient-to-r from-blue-600 via-purple-600 to-blue-600 bg-clip-text text-transparent animate-[mygradient_3s_ease_infinite] rounded-sm hover:from-blue-600/30 hover:via-purple-600/30 hover:to-blue-600/30 dark:hover:from-purple-600/10 dark:hover:via-blue-600/10 dark:hover:to-purple-600/10 hover:bg-clip-padding hover:text-current duration-500',
+                          active ? '' : 'flex sm:hidden font-bold justify-center bg-[length:400px_400px] bg-gradient-to-r from-blue-600 via-purple-600 to-blue-600 bg-clip-text text-transparent animate-[mygradient_3s_ease_infinite] rounded-sm hover:from-blue-600/50 hover:via-purple-600/50 hover:to-blue-600/50 dark:hover:from-purple-600/30 dark:hover:via-blue-600/30 dark:hover:to-purple-600/30 hover:bg-clip-padding hover:text-current duration-500',
                           'block px-4 py-2 text-sm'
                         )}
                       >
