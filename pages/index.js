5a59340a11b9338d6ffd1977938e99a1de55c56d
DKsHs <nicolas.contiero198@gmail.com>
2022-03-30 17:23:03 -0300

    Arrumando importações

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ const Home = () => {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
-                    href="/get_started"
+                    href='#'
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md bg-green-400 hover:bg-green-600 md:py-4 md:text-lg md:px-10"
                   >
                     Get started
